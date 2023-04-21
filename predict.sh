ff131f76f78cf409e01ad7de07374a59cc74d753
Siyuan Peng <peng2000sy@gmail.com>
2023-04-20 23:54:33 -0400

    update raft param

--- predict.sh
+++ predict.sh
@@ -1,7 +1,14 @@
 # python evaluate.py --model models/raft-sintel.pth --dataset ouchi --input_dir "../output/right_down_50"
 python evaluate.py \
-    --model /home/siyuan/research/optical_illusion/checkpoints/train-from-scratch/exp_1/100000_raft-chairsSM.pth \
-    --output_dir /home/siyuan/research/optical_illusion/predictions/ouchi_prediction_exp1_3_iter \
+    --model /home/siyuan/research/optical_illusion/checkpoints/train-from-scratch/exp_0/90000_raft-chairsSM.pth \
+    --output_dir /home/siyuan/research/optical_illusion/predictions/wheel_prediction/exp4_12_iter \
     --dataset ouchi \
-    --iter 3\
-    --input_dir "../ouchi_output/right_down_2"
+    --iter 12\
+    --input_dir "../wheel_output/zoom_in_2"
+
+# python evaluate.py \
+#     --model /home/siyuan/research/optical_illusion/checkpoints/train-from-scratch/exp_0/90000_raft-chairsSM.pth \
+#     --output_dir /home/siyuan/research/optical_illusion/predictions/ouchi_prediction_exp3_12_iter \
+#     --dataset ouchi \
+#     --iter 12\
+#     --input_dir "../ouchi_output/right_down_2"
